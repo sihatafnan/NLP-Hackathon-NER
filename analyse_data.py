ba480ff95e2c322a419ec5e7fe4e917e944a63f8
AfnanCSE98 <sihatafnan15.9.1997@gmail.com>
2023-01-20 10:51:20 +0600

    script_

--- analyse_data.py
+++ analyse_data.py
@@ -3,17 +3,11 @@ file = './data/train.txt'
 # split ny newline
 cnt = 0
 with open(file) as f:
-    # read all lines and split by null character
-    content = f.read().split('\n')
     # split by newline
     str = ""
-    for line in content:
+    for line in f:
         # split by space
-        str += line
-        if line == "":
-            
-            print(line)
-            str = ""
+        print(line)
         cnt+=1
         if cnt ==2:
             break
